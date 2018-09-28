155486024a10ad174c2a5449ad435c5e053e5401
wiradani <adhan432@gmail.com>
2018-09-28 20:50:01 +0700

    init

---
+++ Genus/Assets/buttonexit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonexit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ExitNow()
+    {
+        Application.Quit();
+    }
+}
